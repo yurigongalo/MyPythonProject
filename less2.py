f3f6816b0b922bd33119206e4ee1ec659c171938
yurigongalo <yuri@gongalo.com>
2022-09-02 01:12:35 +0300

    new lesson

---
+++ less2.py
@@ -0,0 +1,23 @@
+# Две переменные int
+a = 1
+print(a)
+aa = 3
+print(aa)
+# Две переменные float
+b = 1.23
+print(b)
+bb = 3.14
+print(bb)
+# Две переменные str
+c = 'Hello'
+print(c)
+cc = "World"
+print(cc)
+# Две переменные bool
+d = True
+print(d)
+dd = False
+print(dd)
+# Переменная с пустым значенимем
+e = None
+print(e)
