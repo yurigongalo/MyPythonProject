1cbdbc063586c1cf547fbf21affd23bade051f34
yurigongalo <yuri@gongalo.com>
2022-09-05 00:11:02 +0300

    home work 11.1

--- hw-less11_1.py
+++ hw-less11_1.py
@@ -1,7 +1,19 @@
-a="2"
-b="0"
+# a=2
+# b="q"
+# try:
+#     c=a+int(b)
+# except ValueError:
+#     print("Вводить разрешено только числа")
+# except ZeroDivisionError:
+#     print("На 0 делить нельзя")
+# except TypeError:
+#     print("Разные типы данных использовать нельзя")
+# else:
+#     print(c)
+a=2
+b="2"
 try:
-    c=int(a)/b
+    c=a+b
 except ValueError:
     print("Вводить разрешено только числа")
 except ZeroDivisionError:
@@ -10,4 +22,5 @@ except TypeError:
     print("Разные типы данных использовать нельзя")
 else:
     print(c)
+
 
