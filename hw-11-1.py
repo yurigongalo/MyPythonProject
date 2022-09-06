49a8db4bbff296c3a9c6438ca094af23cf6b2315
yurigongalo <yuri@gongalo.com>
2022-09-07 01:23:29 +0300

    home work 12-new

--- hw-11-1.py
+++ hw-11-1.py
@@ -1,5 +1,5 @@
-a=int(input())
-b=int(input())
-c=int(input())
+a=int(input("Введите минимальное число"))
+b=int(input("Введите максимальное число"))
+c=int(input("Введите шаг"))
 for i in range(a,b+1,c):
     print((i), end=" ")
