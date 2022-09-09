5e92d8894c5866807e802e6d87993158de5db29f
yurigongalo <yuri@gongalo.com>
2022-09-09 21:41:12 +0300

    я все таки это сделал

--- hw12-1.py
+++ hw12-1.py
@@ -1,7 +1,15 @@
-maximum=0
+# maximum=0
+# number = int(input("Введите число"))
+# while number > 0:
+#    if number%10>maximum:
+#        maximum = number % 10
+#        number = number // 10
+#        print(maximum)
+
 number = int(input("Введите число"))
-while number > 0:
-   if number%10>maximum:
-       maximum = number % 10
-       number = number // 10
-       print(maximum)
+maximum=0
+while maximum <= number %10:
+    if number %10 >= maximum:
+        maximum = number%10
+        number = number//10
+print(maximum)
