9c1d941f37796f5ed4dc1af525784b76e94645ad
yurigongalo <yuri@gongalo.com>
2022-09-24 01:11:18 +0300

    dz16-1-2

--- hw_16_2.py
+++ hw_16_2.py
@@ -7,15 +7,6 @@ for i in range(x):
         a = int(input("Заполните матрицу значениями:"))
         ListA1.append(a)
     ListA.append(ListA1)
-# Поиск максимального числа главной диагонали
-maximum = ListA[0][0]
-for i in ListA:
-    print (i)
-for i in range(0,len(ListA)):
-    if maximum < ListA[i][i]:
-        maximum = ListA[i][i]
-print ("Максимальное число диагонали", maximum)
-# поиск кол-ва отрицательных эл-тов под главной диагональю
 p = 0
 for i in range(0,len(ListA)):
     for j in range(0,len(ListA)):
@@ -24,4 +15,4 @@ for i in range(0,len(ListA)):
         else:
             if ListA[i][j] < 0:
                 p += 1
-print("Колличество отрицательных элементов под главной диагональю",p)
+print(p)
