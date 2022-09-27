3385a973cfd5ccf7586a024739b62d8153212a8e
yurigongalo <yuri@gongalo.com>
2022-09-28 00:33:39 +0300

    dz19-1-22

--- hw_19_2.py
+++ hw_19_2.py
@@ -1,6 +1,14 @@
+import random
 
 n = int(input("Сколько множеств сгенерировать?"))
+print("Сгенерированные множества:")
 for j in range(1,n+1):
     # a = {i for i in range(1,9)}
-    a = {i for i in range(1, 9)}
+    a = {i for i in range(1, random.randint(1,10))}
     print (j,a)
+    print(type(a))
+    print(type(j))
+    for k in range(1,j):
+        if k%3 == 3:
+            print(k)
+    print()
--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-a = (1,2,3,4)
-print(type(a))
-b = 1,2,3,4
-print(type(b))
-c = [1,2,3,4]
-print(type(c))
-d = {1,2,3,4}
-print(type(d))
+# {i**2 % 4 for i in range(10)}
+# {0, 1}
+# print (i)
+# import random
+# print((random.randint(0, 5))*5)
+import numpy
+random_integer_array = numpy.random.random_integers(1, 10, 5)
+print("1-мерный массив случайных целых чисел \n", random_integer_array,"\n")
