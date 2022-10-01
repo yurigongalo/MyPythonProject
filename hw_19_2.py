9a5107771599506ff29fdadf39236cc419dc144b
yurigongalo <yuri@gongalo.com>
2022-10-02 00:36:38 +0300

    dz19-1-22

--- hw_19_2.py
+++ hw_19_2.py
@@ -1,14 +1,28 @@
 import random
-
-n = int(input("Сколько множеств сгенерировать?"))
-print("Сгенерированные множества:")
-for j in range(1,n+1):
-    # a = {i for i in range(1,9)}
-    a = {i for i in range(1, random.randint(1,10))}
-    print (j,a)
-    print(type(a))
-    print(type(j))
-    for k in range(1,j):
-        if k%3 == 3:
-            print(k)
-    print()
+a = set()
+# print(type(a))
+n = int(input("Введите колличество множеств:"))
+m = int(input("Введите колличество элементов каждого множества:"))
+A = []
+for i in range(n):
+    A1 = []
+    for j in range(m):
+        A1.append(random.randint(-50,50))
+    A.append(A1)
+    print('A1: ',A1)
+print('А: ',A)
+B = []
+for i in range(n):
+    B1 = []
+    for j in range(m):
+        if A[i][j]%3 == 0:
+            B1.append(A[i][j])
+        B.append(B1)
+    print('B1:',B1)
+print('B:',B)
+rest1 = set().union(A[0])
+print('Первое множество: ',rest1)
+rest = set().union(*B)
+print('Все элементы, кратные 3м: ',rest)
+print('Элемены, кратные 3м, без первого множества: ',rest-rest1)
+print('Результат 2-м способом',rest.difference(rest1))
