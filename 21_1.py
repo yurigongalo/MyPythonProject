0bfda90f1d8af754e34d2606a9eab51fc664db33
yurigongalo <yuri@gongalo.com>
2022-10-09 09:55:05 +0300

    hw_21-1

---
+++ 21_1.py
@@ -0,0 +1,17 @@
+f = open('class.txt', 'r')
+content = f.read()
+f.close()
+content = content.split("\n")
+pupils = []
+print (content)
+for line in content:
+    line = line.split(" ")
+    pupils.append([line[0], line[1], int(line[2])])
+srednia = 0
+print("Ниже 3 баллов:")
+for p in pupils:
+    srednia += int(p[2])
+    if p[2] < 3:
+        print(f"{p[0]} {p[1]}: {p[2]}")
+srednia /= len(pupils)
+print(f"Средняя оценка по классу: {srednia}")
--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-n = None
-print (n)
+counter = {}
+for i in range(int(input('Введите число: '))):
+    line = input('Введите слова: ').split()
+    for word in line:
+        counter[word] = counter.get(word, 0) + 1
+
+max_count = max(counter.values())
+most_frequent = [k for k, v in counter.items() if v == max_count]
+print(min(most_frequent))
+
