98bbb83808724b6ccf70efa78bd028fb8c150b06
yurigongalo <yuri@gongalo.com>
2022-11-04 00:44:59 +0300

    oop1 - re

--- OOP-1.py
+++ OOP-1.py
@@ -8,4 +8,12 @@ class Person:
         print("привет " + self.name + " ваш возраст " + str(self.age))
 
 human1 = Person(32,"Юра") # Объект на основе класса
+human2 = Person(29,"Анна")
+human3 = Person(4,'Ксения')
+human4 = Person('10 месяцев','Ваня')
+human5 = Person(2,"Мурзик")
 human1.hello() # Метот этого класса
+human2.hello()
+human3.hello()
+human4.hello()
+human5.hello()
