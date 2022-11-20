c80a3b04c01084de7ee1b9cb5007d4e31afa421a
yurigongalo <yuri@gongalo.com>
2022-11-20 14:58:45 +0300

    HW_DB1_WORK

--- hw_db1.py
+++ hw_db1.py
@@ -1,2 +1,8 @@
 import sqlite3
 conn = sqlite3.connect("database.db")
+curs = conn.cursor()
+zap1 = """CREATE TABLE DIRECTORY(FULL_NAME TEXT,ACADEMIC_DEGREE TEXT, SCIENTIFIC_DIRECTION TEXT, PLACE_OF_WORK TEXT, CHAIR TEXT, JOB_TITLE TEXT, COUNTRY TEXT, CITY TEXT, ADDRESS TEXT, WORK_PHONE TEXT, EMAIL TEXT);"""
+curs.execute(zap1)
+conn.commit()
+curs.close()
+conn.close()
