2b56710af08ac8bfd1ebc6d0fd0a470b1179e43d
yurigongalo <yuri@gongalo.com>
2022-11-22 00:58:37 +0300

    HW_DB1_WORKing_finish_tb6

--- hw_db1.py
+++ hw_db1.py
@@ -11,5 +11,35 @@ tb3 = """CREATE TABLE IF NOT EXISTS  information(conference_name TEXT, the_date_
 sponsors TEXT, amount_of_days TEXT, number_of_participants TEXT, number_of_speakers TEXT);"""
 curs.execute(tb3)
 conn.commit()
+ins11 = """INSERT INTO directory(FULL_NAME, ACADEMIC_DEGREE, SCIENTIFIC_DIRECTION, PLACE_OF_WORK, CHAIR, JOB_TITLE, COUNTRY, CITY, \
+ADDRESS, WORK_PHONE, EMAIL) VALUES("Petr Petrov", "Doctor", "psychiatry", "Kobrinskaya CRB", "psychiatry", "Zav Otdeleniem", "Belarus", \
+"Kobrin", "Kobrin, Parkovaya sr, d1", "2-31-31", "petrov@kobincrb.by");"""
+
+ins12 = """INSERT INTO directory(FULL_NAME, ACADEMIC_DEGREE, SCIENTIFIC_DIRECTION, PLACE_OF_WORK, CHAIR, JOB_TITLE, COUNTRY, CITY, \
+ADDRESS, WORK_PHONE, EMAIL) VALUES("Ivan Ivanov", "Doctor", "Terapevt", "Kobrinskaya CRB", "Medicina", "Zam Zav Otdeleniem", "Belarus", \
+"Kobrin", "Kobrin, Parkovaya str, d2", "2-32-32", "ivanov@kobincrb.by");"""
+
+ins13 = """INSERT INTO directory(FULL_NAME, ACADEMIC_DEGREE, SCIENTIFIC_DIRECTION, PLACE_OF_WORK, CHAIR, JOB_TITLE, COUNTRY, CITY, \
+ADDRESS, WORK_PHONE, EMAIL) VALUES("Sidor Sidorov", "Doctor", "Vrach", "Brestskaya CRB", "Medicina", "Zav Otdeleniem", "Belarus", \
+"Brest", "Brest, Parkovaya str, d2", "22-32-32", "sidirov@brestcrb.by");"""
+
+ins14 = """INSERT INTO directory(FULL_NAME, ACADEMIC_DEGREE, SCIENTIFIC_DIRECTION, PLACE_OF_WORK, CHAIR, JOB_TITLE, COUNTRY, CITY, \
+ADDRESS, WORK_PHONE, EMAIL) VALUES("Ivan Sidorov", "Doctor", "Vrach", "Brestskaya CRB", "Medicina", "Zam Zav Otdeleniem", "Belarus", \
+"Brest", "Brest, Parkovaya str, d8", "24-32-32", "sidiroivan@brestcrb.by");"""
+
+ins15 = """INSERT INTO directory(FULL_NAME, ACADEMIC_DEGREE, SCIENTIFIC_DIRECTION, PLACE_OF_WORK, CHAIR, JOB_TITLE, COUNTRY, CITY, \
+ADDRESS, WORK_PHONE, EMAIL) VALUES("Sergey Sergeev", "Doctor", "Vrach", "Pinskaya CRB", "Medicina", "Zav Otdeleniem", "Belarus", \
+"Pinsk", "Pinsk, PPinskaya str, d9", "34-32-32", "sergeev@pinskcrb.by");"""
+
+ins16 = """INSERT INTO directory(FULL_NAME, ACADEMIC_DEGREE, SCIENTIFIC_DIRECTION, PLACE_OF_WORK, CHAIR, JOB_TITLE, COUNTRY, CITY, \
+ADDRESS, WORK_PHONE, EMAIL) VALUES("Pavel Pavlov", "Doctor", "Vrach", "Pinskaya CRB", "Medicina", "Zam Zav Otdeleniem", "Belarus", \
+"Pinsk", "Pinsk, Sovetskaya str, d3", "44-32-34", "pavlov@pinskcrb.by");"""
+curs.execute(ins11)
+curs.execute(ins12)
+curs.execute(ins13)
+curs.execute(ins14)
+curs.execute(ins15)
+curs.execute(ins16)
+conn.commit()
 curs.close()
 conn.close()
